2942f03d86b0f929e5e96d32b87babc13b7f6ebb
th1exterminator <atillmand@gmail.com>
2024-02-06 16:32:04 -0600

    extra

--- sketch_nov8a_buttonslights_20231129163743.ino
+++ sketch_nov8a_buttonslights_20231129163743.ino
@@ -1,8 +1,7 @@
 #include <Adafruit_MCP23X17.h> //Using Adafruit library for MCP23017
 
 Adafruit_MCP23X17 mcp0, mcp1; //Instantiate mcp object
-const static int buttons[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; // Pins on MCP
-const static int lights[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};//Pins on MCP
+const static int pins[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; // Pins on MCP
 
 void setup() {
   Serial.begin(9600);
@@ -10,16 +9,17 @@ void setup() {
   mcp0.setupInterrupts(true, true, LOW);
   mcp1.begin_I2C(0x21);// 001-Gnd,Gnd,5v.
  
-  for(int x = 0;x < sizeof(buttons)/sizeof(int); x++) // Iterates through all buttons
+  for(int x = 0;x < sizeof(pins)/sizeof(int); x++) // Iterates through all buttons
   {
-    mcp0.pinMode(buttons[x], INPUT_PULLUP); // Places Pin "x" into a natural HIGH state
+    mcp0.pinMode(pins[x], INPUT_PULLUP); // Places Pin "x" into a natural HIGH state
   } 
-  for(int x = 0; x < sizeof(lights)/sizeof(int); x++) // Iterates through all LEDs
+  for(int x = 0; x < sizeof(pins)/sizeof(int); x++) // Iterates through all LEDs
   {
-    mcp1.pinMode(lights[x], OUTPUT); // Set LEDs to output
+    mcp1.pinMode(pins[x], OUTPUT); // Set LEDs to output
   } 
 }
 
 void loop() {
   Serial.write(mcp0.getLastInterruptPin());
+  mcp1.writeGPIO(HIGH, mcp0.getLastInterruptPin());
 }
