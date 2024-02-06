a75e9560e252fb4f7a93fcd4672c7ee01a245151
th1exterminator <atillmand@gmail.com>
2024-02-06 16:22:00 -0600

    idk

--- sketch_nov8a_buttonslights_20231129163743.ino
+++ sketch_nov8a_buttonslights_20231129163743.ino
@@ -5,6 +5,7 @@ const static int buttons[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; // Pins on
 const static int lights[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};//Pins on MCP
 
 void setup() {
+  Serial.begin(9600);
   mcp0.begin_I2C(0x20);// 000-Gnd,Gnd,Gnd-using chip Address along with hardwiring to declare these seperately.
   mcp0.setupInterrupts(true, true, LOW);
   mcp1.begin_I2C(0x21);// 001-Gnd,Gnd,5v.
